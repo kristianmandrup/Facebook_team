7508999092d27754c92c0fbf62e2d2bb3107c64a
Kristian Mandrup <kmandrup@gmail.com>
2012-04-02 09:32:49 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookTeam::Application.config.secret_token = 'd402b4835a3133b677daa3a324459d97197ae93a0d40c546989b1811d143a138ab4d56f2ed428fac631ffffc2388f3c7e9f649ffecb308a79e5a682d138b9072'
